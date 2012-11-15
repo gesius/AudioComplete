gesius/AudioComplete

commit c1a4c253d4f18b3aec88c5308e3ef57cef375922

indicate negative TC when edit is positive.
git-svn-id: svn://localhost/ardour2/branches/3.0@13491 d708f5d6-7413-0410-9779-e7cbd77b26cf

gtk2_ardour/audio_clock.cc
@@ -1446,10 +1446,11 @@ AudioClock::on_key_press_event (GdkEventKey* ev)
 	if (edit_is_negative) {
 		edit_string.replace(0,1,"-");
 	} else {
-		/* TODO think about this case.
-		 * The TC will be positive unless the edit is relative.
-		 */
-		edit_string.replace(0,1," ");
+		if (pre_edit_string.at(0) == '-') {
+			edit_string.replace(0,1,"_");
+		} else {
+			edit_string.replace(0,1," ");
+		}
 	}
 
 	show_edit_status (highlight_length);

commit 683cd52302250b7774a41721c8fbfa0554de1809

hotfix negative TC editing - problem was introduced in r13491
git-svn-id: svn://localhost/ardour2/branches/3.0@13506 d708f5d6-7413-0410-9779-e7cbd77b26cf

gtk2_ardour/audio_clock.cc
@@ -1925,8 +1925,8 @@ AudioClock::timecode_validate_edit (const string&)
 	int hours;
 	char ignored[2];
 
-	if (sscanf (_layout->get_text().c_str(), "%" PRId32 ":%" PRId32 ":%" PRId32 "%[:;]%" PRId32,
-		    &hours, &TC.minutes, &TC.seconds, ignored, &TC.frames) != 5) {
+	if (sscanf (_layout->get_text().c_str(), "%[- _]%" PRId32 ":%" PRId32 ":%" PRId32 "%[:;]%" PRId32,
+		    ignored, &hours, &TC.minutes, &TC.seconds, ignored, &TC.frames) != 6) {
 		return false;
 	}
 
@@ -1987,7 +1987,7 @@ AudioClock::frames_from_timecode_string (const string& str) const
 	char ignored[2];
 	int hours;
 
-	if (sscanf (str.c_str(), "%d:%d:%d%[:;]%d", &hours, &TC.minutes, &TC.seconds, ignored, &TC.frames) != 5) {
+	if (sscanf (str.c_str(), "%[- _]%d:%d:%d%[:;]%d", ignored, &hours, &TC.minutes, &TC.seconds, ignored, &TC.frames) != 6) {
 		error << string_compose (_("programming error: %1 %2"), "badly formatted timecode clock string", str) << endmsg;
 		return 0;
 	}